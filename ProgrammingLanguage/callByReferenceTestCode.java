ca12218c8425be15369015146eb458ec69e49838
ssffwert <ssffwert@naver.com>
2021-11-30 18:13:38 +0900

    테스트코드 추가

--- ProgrammingLanguage/callByReferenceTestCode.java
+++ ProgrammingLanguage/callByReferenceTestCode.java
@@ -2,46 +2,57 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class callByReferenceTestCode {
+
+	public static void assignNumber(NumberClass num) {
+		num.mNum = 10;
+	}
 	
 	public static void assignNewNumber(NumberClass num) {
 		num = new NumberClass(20);
 	}
-	
+
 	public static void changeNumber(NumberClass num) {
 		num.setNumber(30);
 	}
-	
+
 	public static void changeMap(Map map) {
 		map.put("num", 2);
 	}
-	
+
 	public static void main(String[] args) {
-		NumberClass mainNum = new NumberClass(10);
-		assignNewNumber(mainNum);
-		System.out.println("mainNum.getNumber():" + mainNum.getNumber());
+		NumberClass mainNum = new NumberClass(1);
+		System.out.println("init mainNum.getNumber():" + mainNum.getNumber());
+
+		assignNumber(mainNum);
+		System.out.println("after assignNumber(), mainNum.getNumber():" + mainNum.getNumber());
 		
+		assignNewNumber(mainNum);
+		System.out.println("after assignNewNumber(), mainNum.getNumber():" + mainNum.getNumber());
+
 		changeNumber(mainNum);
-		System.out.println("mainNum.getNumber():" + mainNum.getNumber());
+		System.out.println("after changeNumber(), mainNum.getNumber():" + mainNum.getNumber());
+		System.out.println();
+		
 		
 		Map map = new HashMap();
 		map.put("num", 1);
-		System.out.println("map.get(\"num\"):" + map.get("num"));
+		System.out.println("init map.get(\"num\"):" + map.get("num"));
 		changeMap(map);
-		System.out.println("map.get(\"num\"):" + map.get("num"));
+		System.out.println("after changeMap(), map.get(\"num\"):" + map.get("num"));
 	}
 }
 
 class NumberClass {
-	private int mNum;
-	
+	public int mNum;
+
 	public NumberClass(int num) {
 		mNum = num;
 	}
-	
+
 	public int getNumber() {
 		return mNum;
 	}
-	
+
 	public void setNumber(int num) {
 		mNum = num;
 	}
